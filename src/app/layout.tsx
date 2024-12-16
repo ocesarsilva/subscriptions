9def79ec504d209d06128d7f29b7dfe15f8814a2
Cesar Silva <cesarsilva.dev@gmail.com>
2024-12-15 21:15:08 -0300

    feat: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
+import { geistMono, geistSans } from "@/lib/fonts"
+import { cn } from "@/lib/utils"
+import "@/styles/globals.css"
+
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +15,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR" suppressHydrationWarning>
+      <head />
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          geistSans.variable,
+          geistMono.variable
+        )}
       >
         {children}
       </body>
